f099080494d8d2f85114c3c5a348bcd35e70073f
Katy <surekaty0919@gmail.com>
2025-08-11 10:03:09 -0400

    change tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "文化影響力平台",
   description: "因為我們擁有共同的文化記憶，所以我們成為一家人。",
+  icons: {
+    icon: "/icons/logo_tab.png",
+    shortcut: "/icons/logo_tab.png",
+    apple: "/icons/logo_tab.png",
+  },
 };
 
 export default function RootLayout({
