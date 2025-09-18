3e2e3e63c8d4621d1de5f4e7eff317f81ce24924
Katy <surekaty0919@gmail.com>
2025-09-18 17:31:36 -0400

    update partner card icon

--- src/app/(pages)/partner/page.tsx
+++ src/app/(pages)/partner/page.tsx
@@ -3,16 +3,16 @@
 import { PageLayout, PartnerCard } from '@/components';
 
 const partners = [
-  { id: 1, name: "Apple", logo: "/logos/apple.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'erewrwerwerwerwe' },
-  { id: 2, name: "Microsoft", logo: "/logos/microsoft.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rewrwer' },
-  { id: 3, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rewrwerwr' },
-  { id: 4, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 5, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 6, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 7, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 8, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 9, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
-  { id: 10, name: "Google", logo: "/logos/google.png", link: '', picture: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80', description: 'rwerewrwer' },
+  { id: 1, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'erewrwerwerwerwe' },
+  { id: 2, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rewrwer' },
+  { id: 3, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rewrwerwr' },
+  { id: 4, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 5, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 6, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 7, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 8, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 9, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
+  { id: 10, name: "財團法人玉溪有容教育基金會", link: 'https://www.garden91.org/zh-TW', picture: '/icons/Partner_logo.png', description: 'rwerewrwer' },
 ];
 
 export default function PartnersPage() {
